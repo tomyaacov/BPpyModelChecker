50ed4b847d33316619af7dde8cec59ae1683a4cd
tomyaacov <tomyaacov1210@gmail.com>
2023-05-23 16:29:22 +0300

    improve experiment output writing

--- scripts/run_all_bppy_bounded.sh
+++ scripts/run_all_bppy_bounded.sh
@@ -14,6 +14,7 @@
 module load anaconda ### load anaconda module
 source activate bppy_model_checking ### activating Conda environment. Environment must be configured before running the job
 cd ~/repos/BPpyModelChecker/ || exit
+ulimit -s
 options=("hot_cold2 30 1 1" "hot_cold2 60 1 1" "hot_cold2 90 1 1")
 #options=(
 #
